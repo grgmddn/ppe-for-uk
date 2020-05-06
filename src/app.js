1febc5a22ae784f911beed8718bde46913867a48
Léa Subrenat <subrlea@yahoo.fr>
2020-05-06 18:25:16 -0500

    Move cookieConsent func from app.js

--- src/app.js
+++ src/app.js
@@ -1,42 +1,7 @@
-import Cookies from 'js-cookie';
-
 import 'reset-css';
 import './styles/main.scss';
 
 const runTime = new Date();
-
-export const setCookieConsent = () => {
-  let key = 'cookie_consent_accepted';
-  let cookieConsentBanner = document.getElementById('cookieConsent');
-  let cookieConsentAcceptButton = document.getElementById('cookieConsentAccept');
-
-  if (cookieConsentBanner && cookieConsentAcceptButton) {
-
-    if (Cookies.get(key)) {
-
-      cookieConsentBanner.classList.add('is-hidden');
-
-    } else {
-
-      cookieConsentBanner.classList.remove('is-hidden');
-
-      cookieConsentAcceptButton.addEventListener('click', () => {
-
-        Cookies.set(key, true, { expires: 180 });
-
-        if (Cookies.get(key)) {
-          cookieConsentBanner.classList.add('is-hidden');
-        } else {
-          console.warn('Unable set cookies.');
-        }
-      });
-
-    }
-
-  } else {
-    console.warn('No cookie consent banner!');
-  }
-};
 
 const scrollTo = (element) => {
 
